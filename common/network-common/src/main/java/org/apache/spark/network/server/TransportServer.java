faf367e4b2cd1e6e4f03c8bd54710a36cda67d33
redscarf <hongweijin1993@gmail.com>
2020-05-21 21:51:23 +0800

    rpc read

--- common/network-common/src/main/java/org/apache/spark/network/server/TransportServer.java
+++ common/network-common/src/main/java/org/apache/spark/network/server/TransportServer.java
@@ -89,6 +89,7 @@ public class TransportServer implements Closeable {
     IOMode ioMode = IOMode.valueOf(conf.ioMode());
     EventLoopGroup bossGroup =
       NettyUtils.createEventLoop(ioMode, conf.serverThreads(), conf.getModuleName() + "-server");
+    // 为啥两个 group 一样呢？有什么特殊意义嘛？
     EventLoopGroup workerGroup = bossGroup;
 
     PooledByteBufAllocator allocator = NettyUtils.createPooledByteBufAllocator(
